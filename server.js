75e68329fe2055db5bd61e49e8bf2b53e315878b
luanaflorencio <lua.m.016@gmail.com>
2019-07-08 00:30:09 -0300

    Modificação da pasta

--- server.js
+++ server.js
@@ -5,22 +5,22 @@ var path = require("path");
 const app = express();
 app.use(session({ secret: 'XXassasas¨¨$', resave: false, saveUninitialized: true }));
 
-app.use(express.static(__dirname + '/pug'));
+app.use(express.static(__dirname + '/html'));
 
 app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname+'/html/index.pug'));
+  res.sendFile(path.join(__dirname+'/html/index.html'));
 })
 
 app.get('/comojogar', (req, res, next) => {
-  res.sendFile(path.join(__dirname+'/html/comojogar.pug'));
+  res.sendFile(path.join(__dirname+'/html/comojogar.html'));
 })
 
 app.get('/jogar', (req, res, next) => {
-  res.sendFile(path.join(__dirname+'/html/jogar.pug'));
+  res.sendFile(path.join(__dirname+'/html/jogar.html'));
 })
 
 app.get('/sobre', (req, res, next) => {
-  res.sendFile(path.join(__dirname+'/html/sobre.pug'));
+  res.sendFile(path.join(__dirname+'/html/sobre.html'));
 })
 
 app.listen(3000, () => {
